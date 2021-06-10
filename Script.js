11d74ab1a0dfd451534a7e80617f3f315ea6b959
Irastus Jasper <jasperirastus@gmail.com>
2021-06-09 21:15:20 -0700

    value and variable

--- Script.js
+++ Script.js
@@ -1,2 +1,21 @@
 let js='Amazing';
 console.log(40+10+8-10)
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Irastus";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let kasper_jasper = "kj";
+let _function = 27;
+let $funtion = 27;
+let PI = 3.14;
+
+let myFirstJob = "Coder";
+let myCurretJob = "teacher"
+
+console.log(myFirstJob);
