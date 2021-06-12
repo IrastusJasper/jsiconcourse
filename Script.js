0ecc221a246e7d31584b8b935b884f47705e0e73
Irastus Jasper <jasperirastus@gmail.com>
2021-06-11 21:18:40 -0700

    Data type

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+/*
 let js='Amazing';
 console.log(40+10+8-10)
 
@@ -19,3 +20,31 @@ let myFirstJob = "Coder";
 let myCurretJob = "teacher"
 
 console.log(myFirstJob);
+
+let myCountry = "India";
+let myContinent = "Asia";
+
+console.log(myCountry);
+console.log(myContinent);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 18);
+console.log(typeof 'Jasper');
+
+javascriptIsFun ="yes";
+console.log(typeof javascriptIsFun);
+
+//undefine
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2003;
+console.log(typeof year);
+
+console.log(typeof null);
