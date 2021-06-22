93fc5483f5986c2210d828b5d25f522007d15f9b
Irastus Jasper <jasperirastus@gmail.com>
2021-06-22 20:39:22 +0530

    Truthy and falsy values & Equality Operators

--- Script.js
+++ Script.js
@@ -187,7 +187,7 @@ if(markBMI > johnBMI){
 }
 else{
     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
-}*/
+}
 
 // type conversion
 const inputYear = '2003';
@@ -205,4 +205,49 @@ console.log('23' > '18');
 
 let n ='1' + 1;// '11'
 n = n - 1;
-console.log(n);
+console.log(n);*/
+
+// 5 falsy values: 0, '',undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jasper'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 2;
+if(money){
+    console.log("Don't spend it all");
+}else{
+    console.log('You should got a job');
+}
+
+let height;
+if (height){
+    console.log('YAY! Height is defined');
+}else{
+    console.log('Height is defined');
+}
+
+// Equality operator
+
+const age = '18';
+if(age === 18) console.log('You just became an adult (Strict)');
+
+if(age == 18) console.log('You just became an adult (Loose)');
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if(favourite === 23){ // 23 === 23 ->true
+    console.log('cool! 23 is an amzaing number!');
+}else if(favourite === 7){
+    console.log('7 is also a cool number');
+}else if(favourite === 9){
+    console.log('9 is also a cool number');
+}else{
+    console.log('Number is not 23 or 7 or 9');
+}
+
+if(favourite !== 23) console.log('Why not 23?');
