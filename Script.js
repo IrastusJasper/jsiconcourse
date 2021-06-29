a14fafbe0ce3dfe1a7645dfd22cf5913529d015f
Irastus Jasper <jasperirastus@gmail.com>
2021-06-29 20:13:57 +0530

    Function Declaration & Expressions Arrow Function

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+/*
 function logger(){
     console.log('My name is Jasper');
 }
@@ -18,3 +19,33 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+// Function declaration
+function calAge1(birthYear){
+    return 2021 - birthYear;
+}
+const age1 = calAge1(2003);
+
+// Function Expression
+
+const calAge2 = function(birthYear){
+    return 2021 - birthYear;
+}
+const age2 = calAge2(2003   );
+
+console.log(age1, age2);
+
+// arrow function
+
+const calAge3 = birthYear => 2021 - birthYear;
+const age3 = calAge3(2003);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName)  => {
+    const age = 2021 - birthYear;
+    const retirement = 60 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearUntilRetirement(2003, 'Jasper'));
+console.log(yearUntilRetirement(2005, 'Kasper'));
