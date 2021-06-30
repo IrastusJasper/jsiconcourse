8ac23e247dfa21fc470dafa703334ee46a9512f6
Irastus Jasper <jasperirastus@gmail.com>
2021-06-30 20:13:13 +0530

    Function calling other functions & Reviewing

--- Script.js
+++ Script.js
@@ -19,7 +19,7 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
-*/
+
 // Function declaration
 function calAge1(birthYear){
     return 2021 - birthYear;
@@ -49,3 +49,41 @@ const yearUntilRetirement = (birthYear, firstName)  => {
 }
 console.log(yearUntilRetirement(2003, 'Jasper'));
 console.log(yearUntilRetirement(2005, 'Kasper'));
+*/
+
+// function calling other functions
+
+function cutFruitPieces(fruit){
+    return fruit * 3;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePiceces = cutFruitPieces(oranges);
+
+    const juice =`Juice with ${applePieces} pieces of apple and ${orangePiceces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+// Reviewing the function
+
+const calAge = function (birthYear){
+    return 2021 - birthYear;
+}
+
+const yearUntilRetirement = function(birthYear, firstName)  {
+    const age = calAge(birthYear);
+    const retirement = 60 - age;
+
+    if(retirement >  0){
+        console.log (`${firstName} retires in ${retirement} years`);
+        return retirement;
+    }else{
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+}
+console.log(yearUntilRetirement(1997, 'John'));
+console.log(yearUntilRetirement(1955, 'mike'));
